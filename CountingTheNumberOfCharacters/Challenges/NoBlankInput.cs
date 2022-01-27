6565e047e4ae6d3e359e4a948fdb44a4db2cd610
Jacob Netz <jlnetz@gmail.com>
2022-01-27 12:20:30 -0600

    Add challenge solution 'no blank input'

---
+++ CountingTheNumberOfCharacters/Challenges/NoBlankInput.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace CountingTheNumberOfCharacters.Challenges;
+
+internal class NoBlankInput
+{
+    public static void Execute()
+    {
+        WriteLine("What is the input string? ");
+        var input = ReadLine();
+        while (input is not { Length: > 0 })
+        {
+            WriteLine("Your input must contain at least one character, please try again: ");
+            input = ReadLine();
+        }
+        WriteLine($"Your input \"{input}\" contains {input?.Length} characters");
+    }
+}
