ad08798032c79be854a002f07e5c23297682a133
Jacob Netz <jlnetz@gmail.com>
2022-01-28 12:20:41 -0600

    Add solution for 'Mad Libs' exercise

---
+++ MadLibs/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+Write("Enter a noun: ");
+var noun = ReadLine();
+Write("Enter a verb: ");
+var verb = ReadLine();
+Write("Enter an adjective: ");
+var adjective = ReadLine();
+Write("Enter an adverb: ");
+var adverb = ReadLine();
+WriteLine($"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!");
