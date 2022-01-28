7e8e4c398c50b502b3a935eee017a5062cf13813
Jacob Netz <jlnetz@gmail.com>
2022-01-28 13:07:21 -0600

    Add 'Branching Story' challenge solution

---
+++ MadLibs/Challenges/BranchingStory.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace MadLibs.Challenges;
+
+internal class BranchingStory
+{
+    public static void Execute()
+    {
+        Write("Enter a noun: ");
+        var noun = ReadLine();
+        Write("Enter a verb: ");
+        var verb = ReadLine();
+        Write("Enter an adjective: ");
+        var adjective = ReadLine();
+        Write("Enter an adverb: ");
+        var adverb = ReadLine();
+        WriteLine($"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!");
+        WriteLine(noun?.ToLowerInvariant() == "dog" ? "Everybody loves dogs!" : "Who doesn't love cats!");
+    }
+}
