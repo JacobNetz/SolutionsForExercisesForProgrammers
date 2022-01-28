bd687b7361eafcc4a102ed58f64de7973874084c
Jacob Netz <jlnetz@gmail.com>
2022-01-28 08:13:14 -0600

    Add 'Quote List' challenge solution

---
+++ PrintingQuotes/Challenges/QuoteList.cs
@@ -0,0 +1,17 @@
+﻿public record QuoteInfo(string Quote, string Author);
+
+internal class QuoteList
+{
+    public static void Execute()
+    {
+        var quotes = new List<QuoteInfo>
+        {
+            new("Keep your stick on the ice", "Red Green"),
+            new("Here's looking at you, kid.","Rick Blaine"),
+            new("Rosebud","Charles Foster Kane"),
+            new("May the Force be with you.","Han Solo")
+        };
+        foreach (var (quote, author) in quotes)
+            Console.WriteLine($"{author} says, \"{quote}\"");
+    }
+}
