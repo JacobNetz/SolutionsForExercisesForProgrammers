c9be245c262fba322f357cee6ba52eb629a8e12a
Jacob Netz <jlnetz@gmail.com>
2022-01-28 12:43:00 -0600

    Add 'More Inputs' challenge solution

---
+++ MadLibs/Challenges/MoreInputs.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+internal class MoreInputs
+{
+    public static void Execute()
+    {
+        Write("Enter a noun: ");
+        var noun = ReadLine();
+        Write("Enter a name (noun): ");
+        var name = ReadLine();
+        Write("Enter a verb: ");
+        var verb = ReadLine();
+        Write("Enter a verb ending in 'ing': ");
+        var verbIng = ReadLine();
+        Write("Enter an adjective: ");
+        var adjective = ReadLine();
+        Write("Enter an adverb: ");
+        var adverb = ReadLine();
+        WriteLine($"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!");
+        WriteLine($"You should try {verbIng} with {name}.");
+    }
+}
