92c750d794cd5d571e0bb779b705d58ab9b08811
Jacob Netz <jlnetz@gmail.com>
2022-01-31 10:34:19 -0600

    Add input/output for 'Area of a Rectangular Room' exercise

--- AreaOfARectangularRoom/AreaOfARectangularRoom/Program.cs
+++ AreaOfARectangularRoom/AreaOfARectangularRoom/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace AreaOfARectangularRoom;
+﻿using AreaOfARectangularRoom;
+using static System.Console;
 
-public class Program
-{
-    static void Main(string[] args)
-    {
-        
-    }
-}
+Write("What is the length of the room in feet? ");
+var length = Convert.ToDouble(ReadLine());
+Write("What is the width of the room in feet? ");
+var width = Convert.ToDouble(ReadLine());
+WriteLine($"You entered dimensions of {length} feet by {width} feet.");
+
+var (feet, meters) = new AreaCalculator().CalculateArea(length, width);
+WriteLine("The area is");
+WriteLine($"{feet} square feet");
+WriteLine($"{meters} square meters");
