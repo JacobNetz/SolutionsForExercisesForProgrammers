15789aaf02f4aa74fda42902a01f440bf3760c54
Jacob Netz <jlnetz@gmail.com>
2022-02-08 08:17:02 -0600

    Add console input/output functionality for 'CurrencyConversion' exercise

--- CurrencyConversion/CurrencyConversion/Program.cs
+++ CurrencyConversion/CurrencyConversion/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("How many euros are you exchanging? ");
+var euros = ReadLine();
+Write("What is the exchange rate? ");
+var exchangeRate = ReadLine();
+
+var converter = new CurrencyConverter.CurrencyConverter();
+var dollars = converter.ConvertCurrency(Convert.ToDouble(euros), Convert.ToDouble(exchangeRate));
+
+WriteLine($"{euros} euros at an exchange rate of {exchangeRate} is {dollars} U.S. dollars.");
