d7aabadd7a51565d6704f365ecb15f27b7a96284
Jacob Netz <jlnetz@gmail.com>
2022-02-15 08:00:38 -0600

    Add tests and calculator functionality for 'DeterminingCompoundInterest' exercise

--- DeterminingCompoundInterest/InterestCalculator.Tests/CompoundInterestCalculatorTests.cs
+++ DeterminingCompoundInterest/InterestCalculator.Tests/CompoundInterestCalculatorTests.cs
@@ -5,8 +5,28 @@ namespace InterestCalculator.Tests;
 public class CompoundInterestCalculatorTests
 {
     [Fact]
-    public void Test1()
+    public void CalculateTotalValue_WithValidInputs_ShouldCalculateCorrectTotal()
     {
+        var calculator = new CompoundInterestCalculator();
 
+        var total = calculator.CalculateTotalValue(1500, 4.3f, 6, 4);
+
+        Assert.Equal(1938.84, total);
+    }
+
+    [Theory]
+    [InlineData(1938.8394775390625, 1938.84)]
+    [InlineData(1938.84, 1938.84)]
+    [InlineData(1938.839, 1938.84)]
+    [InlineData(1938.831, 1938.84)]
+    [InlineData(1938.830, 1938.83)]
+    [InlineData(1938, 1938)]
+    public void RoundUSDToNearestPenny_WithFractionalPennies_ShouldRoundUpToTheNextPenny(double original, double expected)
+    {
+        var calculator = new CompoundInterestCalculator();
+
+        var total = calculator.RoundUSDToNearestPenny(original);
+
+        Assert.Equal(expected, total);
     }
 }
--- DeterminingCompoundInterest/InterestCalculator/CompoundInterestCalculator.cs
+++ DeterminingCompoundInterest/InterestCalculator/CompoundInterestCalculator.cs
@@ -1,5 +1,27 @@
 ﻿namespace InterestCalculator;
 public class CompoundInterestCalculator
 {
+    /// <summary>
+    /// Calculates total value of investment, using formula A = P(1 + r/n)^nt
+    /// </summary>
+    /// <param name="principal"></param>
+    /// <param name="interestRate">as a percentage, i.e. 15 (not .15)</param>
+    /// <param name="years"></param>
+    /// <param name="compoundsPerYear">number of times the interest is compounded per year</param>
+    /// <returns></returns>
+    public double CalculateTotalValue(double principal, double interestRate, double years, double compoundsPerYear)
+    {
+        var i = 1 + interestRate / 100 / compoundsPerYear;
+        var j = Math.Pow(i, compoundsPerYear * years);
+        var total = principal * j;
+        var roundedTotal = RoundUSDToNearestPenny(total);
+        return roundedTotal;
+    }
 
+    public double RoundUSDToNearestPenny(double total)
+    {
+        var shiftedValue = total * 100;
+        var roundedValue = Math.Ceiling(shiftedValue);
+        return roundedValue / 100;
+    }
 }
