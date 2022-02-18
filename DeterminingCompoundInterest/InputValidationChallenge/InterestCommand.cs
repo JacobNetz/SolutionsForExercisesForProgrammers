39045cbf14944e91d2325b6b352a111504495cbe
Jacob Netz <jlnetz@gmail.com>
2022-02-18 15:50:30 -0600

    Add CLI solution for 'Input Validation' challenge

---
+++ DeterminingCompoundInterest/InputValidationChallenge/InterestCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using InterestCalculator;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace InputValidationChallenge;
+public class InterestCommand : Command<InterestCommand.Settings>
+{
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "<Principal>")]
+        public double Principal { get; init; }
+        [CommandArgument(1, "<InterestRate>")]
+        public double InterestRate { get; init; }
+        [CommandArgument(2, "<Years>")]
+        public double Years { get; init; }
+        [CommandArgument(3, "<CompoundsPerYear>")]
+        public double CompoundsPerYear { get; init; }
+    }
+
+    public override int Execute([NotNull]CommandContext context, [NotNull] Settings settings)
+    {
+        var calculator = new CompoundInterestCalculator();
+        var total = calculator.CalculateTotalValue(settings.Principal, settings.InterestRate, settings.Years, settings.CompoundsPerYear);
+        AnsiConsole.MarkupLine($"{settings.Principal:C} invested at {settings.InterestRate / 100:P} for {settings.Years} year(s) " +
+                               $"compounded {settings.CompoundsPerYear} times per year is [underline green]{total:C}[/].");
+        return 0;
+    }
+}
--- DeterminingCompoundInterest/InputValidationChallenge/Program.cs
+++ DeterminingCompoundInterest/InputValidationChallenge/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InputValidationChallenge;
+using Spectre.Console.Cli;
+
+// This version of the exercise solution accepts command line arguments
+// instead of prompting for values once the app is running.
+// For example: 
+/*
+> .\InputValidationChallenge.exe -h
+USAGE:
+    InputValidationChallenge.dll <Principal> <InterestRate> <Years> <CompoundsPerYear> [OPTIONS]
+
+ARGUMENTS:
+    <Principal>
+    <InterestRate>
+    <Years>
+    <CompoundsPerYear>
+
+OPTIONS:
+    -h, --help    Prints help information
+
+> InputValidationChallenge.exe 1500 4.3 6 4
+$1,500.00 invested at 4.30% for 6 year(s) compounded 4 times per year is $1,938.84.
+*/
+var app = new CommandApp<InterestCommand>();
+return app.Run(args);
