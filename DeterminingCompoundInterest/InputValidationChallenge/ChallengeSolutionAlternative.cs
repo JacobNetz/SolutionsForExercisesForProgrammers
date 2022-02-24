03a10e0d8284d65b7421e3ea6982302534ef0228
Jacob Netz <jlnetz@gmail.com>
2022-02-24 12:57:48 -0600

    Add custom input validation and colors to 'Input Validation' challenge

--- DeterminingCompoundInterest/InputValidationChallenge/ChallengeSolutionAlternative.cs
+++ DeterminingCompoundInterest/InputValidationChallenge/ChallengeSolutionAlternative.cs
@@ -4,17 +4,35 @@ using Spectre.Console;
 namespace InputValidationChallenge;
 internal class ChallengeSolutionAlternative
 {
+    private const string ErrorMessage = "[red]That is not a valid number, please try again[/]";
+
     public void CalculateInterest()
     {
-        var principal = AnsiConsole.Ask<double>("What is the principal amount?");
-        var rate = AnsiConsole.Ask<double>("What is the rate?");
-        var years = AnsiConsole.Ask<double>("What is the number of years?");
-        var compounds = AnsiConsole.Ask<double>("What is the number of times the interest is compounded per year?");
+        var principal = AnsiConsole.Prompt(
+            new TextPrompt<double>("What is the principal amount?")
+                .PromptStyle("blue")
+                .ValidationErrorMessage(ErrorMessage)
+        );
+        var rate = AnsiConsole.Prompt(
+            new TextPrompt<double>("What is the rate?")
+                .PromptStyle("olive")
+                .ValidationErrorMessage(ErrorMessage)
+        );
+        var years = AnsiConsole.Prompt(
+            new TextPrompt<double>("What is the number of years?")
+                .PromptStyle("fuchsia")
+                .ValidationErrorMessage(ErrorMessage)
+        );
+        var compounds = AnsiConsole.Prompt(
+            new TextPrompt<double>("What is the number of times the interest is compounded per year?")
+                .PromptStyle("teal")
+                .ValidationErrorMessage(ErrorMessage)
+        );
 
         var calculator = new CompoundInterestCalculator();
         var total = calculator.CalculateTotalValue(principal, rate, years, compounds);
 
-        AnsiConsole.WriteLine($"{principal:C} invested at {rate / 100:P} for {years} year(s) " +
-                              $"compounded {compounds} times per year is {total:C}.");
+        AnsiConsole.MarkupLine($"\n[blue]{principal:C}[/] invested at [olive]{rate / 100:P}[/] for [fuchsia]{years}[/] year(s) " +
+                               $"compounded [teal]{compounds}[/] times per year is [lime underline]{total:C}[/].");
     }
 }
