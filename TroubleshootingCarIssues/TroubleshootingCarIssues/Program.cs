f2498f0d8903440e5116fcb1d8e2ca38019a38e9
Jacob Netz <jlnetz@gmail.com>
2022-03-09 12:07:05 -0600

    Add basic solution for 'Troubleshooting Car Issues' exercise

--- TroubleshootingCarIssues/TroubleshootingCarIssues/Program.cs
+++ TroubleshootingCarIssues/TroubleshootingCarIssues/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (Prompt("Is the car silent when you turn the key"))
+{
+    if (Prompt("Are the battery terminals corroded"))
+        Console.WriteLine("Clean terminals and try starting again.");
+    else
+        Console.WriteLine("Replace cables and try again.");
+}
+else
+{
+    if(Prompt("Does the car make a clicking noise"))
+        Console.WriteLine("Replace the battery.");
+    else
+    {
+        if(Prompt("Does the car crank up but fail to start"))
+            Console.WriteLine("Check spark plug connections.");
+        else
+        {
+            if (Prompt("Does the engine start and then die"))
+            {
+                if(Prompt("Does your car have fuel injection"))
+                    Console.WriteLine("Get it in for service.");
+                else
+                    Console.WriteLine("Check to ensure the choke is opening and closing.");
+            }
+        }
+    }
+}
+
+bool Prompt(string promptText)
+{
+    Console.Write($"{promptText}? ");
+    var answer = Console.ReadLine()?.ToLowerInvariant();
+    return answer == null || !answer.StartsWith("n");
+}
