49c3d20d4c12f7bbd6122df4c07f8718b593d25b
Jacob Netz <jlnetz@gmail.com>
2022-03-09 12:17:31 -0600

    Increase readability of basic solution for 'Troubleshooting Car Issues' exercise

--- TroubleshootingCarIssues/TroubleshootingCarIssues/Program.cs
+++ TroubleshootingCarIssues/TroubleshootingCarIssues/Program.cs
@@ -1,26 +1,26 @@
-﻿if (Prompt("Is the car silent when you turn the key"))
+﻿using static System.Console;
+
+if (Prompt("Is the car silent when you turn the key"))
 {
-    if (Prompt("Are the battery terminals corroded"))
-        Console.WriteLine("Clean terminals and try starting again.");
-    else
-        Console.WriteLine("Replace cables and try again.");
+    WriteLine(Prompt("Are the battery terminals corroded")
+        ? "Clean terminals and try starting again."
+        : "Replace cables and try again.");
 }
 else
 {
     if(Prompt("Does the car make a clicking noise"))
-        Console.WriteLine("Replace the battery.");
+        WriteLine("Replace the battery.");
     else
     {
         if(Prompt("Does the car crank up but fail to start"))
-            Console.WriteLine("Check spark plug connections.");
+            WriteLine("Check spark plug connections.");
         else
         {
             if (Prompt("Does the engine start and then die"))
             {
-                if(Prompt("Does your car have fuel injection"))
-                    Console.WriteLine("Get it in for service.");
-                else
-                    Console.WriteLine("Check to ensure the choke is opening and closing.");
+                WriteLine(Prompt("Does your car have fuel injection")
+                    ? "Get it in for service."
+                    : "Check to ensure the choke is opening and closing.");
             }
         }
     }
@@ -28,7 +28,7 @@ else
 
 bool Prompt(string promptText)
 {
-    Console.Write($"{promptText}? ");
-    var answer = Console.ReadLine()?.ToLowerInvariant();
+    Write($"{promptText}? ");
+    var answer = ReadLine()?.ToLowerInvariant();
     return answer == null || !answer.StartsWith("n");
 }
