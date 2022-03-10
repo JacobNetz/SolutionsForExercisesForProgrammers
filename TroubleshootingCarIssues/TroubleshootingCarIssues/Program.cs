4de07358c4cea0c116a7c05342fe6b5ba6576e97
Jacob Netz <jlnetz@gmail.com>
2022-03-10 07:46:06 -0600

    Convert CLI interactions to use Spectre.Console for 'Troubleshooting Car Issues' exercise

--- TroubleshootingCarIssues/TroubleshootingCarIssues/Program.cs
+++ TroubleshootingCarIssues/TroubleshootingCarIssues/Program.cs
@@ -1,34 +1,27 @@
-﻿using static System.Console;
+﻿using static Spectre.Console.AnsiConsole;
 
-if (Prompt("Is the car silent when you turn the key"))
+if (Confirm("Is the car silent when you turn the key"))
 {
-    WriteLine(Prompt("Are the battery terminals corroded")
+    WriteLine(Confirm("Are the battery terminals corroded")
         ? "Clean terminals and try starting again."
         : "Replace cables and try again.");
 }
 else
 {
-    if(Prompt("Does the car make a clicking noise"))
+    if(Confirm("Does the car make a clicking noise"))
         WriteLine("Replace the battery.");
     else
     {
-        if(Prompt("Does the car crank up but fail to start"))
+        if(Confirm("Does the car crank up but fail to start"))
             WriteLine("Check spark plug connections.");
         else
         {
-            if (Prompt("Does the engine start and then die"))
+            if (Confirm("Does the engine start and then die"))
             {
-                WriteLine(Prompt("Does your car have fuel injection")
+                WriteLine(Confirm("Does your car have fuel injection")
                     ? "Get it in for service."
                     : "Check to ensure the choke is opening and closing.");
             }
         }
     }
-}
-
-bool Prompt(string promptText)
-{
-    Write($"{promptText}? ");
-    var answer = ReadLine()?.ToLowerInvariant();
-    return answer == null || !answer.StartsWith("n");
 }
