c09ed6a86765cb806838f0eb0e3e47fccee4aa18
Jacob Netz <jlnetz@gmail.com>
2022-04-22 14:41:05 -0500

    Fix failing tests by converting record structs to record classes and allowing mutablility for Text field for 'Mermaid Parser' challenge

--- TroubleshootingCarIssues/Challenges/MermaidParser/MermaidParser/GraphRecords.cs
+++ TroubleshootingCarIssues/Challenges/MermaidParser/MermaidParser/GraphRecords.cs
@@ -1,12 +1,15 @@
 ﻿namespace MermaidParser;
 
-public readonly record struct Graph(Node Root);
+public record Graph(Node Root);
 
-public record struct Node(
+public record Node(
     string Id,
     string Text,
     string ParentEdgeText,
     IList<Node> Nodes)
 {
+    // Text needs to be mutable so that an already defined node can have its Text updated
+    // if there are future lines that change it's configuration
+    public string Text { get; set; } = Text;
     public bool IsTerminalNode => !Nodes.Any();
 }
